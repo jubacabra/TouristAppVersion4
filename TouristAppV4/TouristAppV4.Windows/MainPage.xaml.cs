de46706760676dd9af0f1a935cf1170cd056f0c1
Jubacabra <jubacabra@live.dk>
2014-11-25 08:28:55 +0100

    Added Animations

--- TouristAppV4/TouristAppV4.Windows/MainPage.xaml.cs
+++ TouristAppV4/TouristAppV4.Windows/MainPage.xaml.cs
@@ -26,5 +26,84 @@ namespace TouristAppV4
         {
             this.InitializeComponent();
         }
+
+        private void MainButton1_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton2_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton3_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton4_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton5_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton6_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton7_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton8_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton9_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void MainButton10_Click(object sender, RoutedEventArgs e)
+        {
+            Image1.Visibility = Visibility.Visible;
+            this.Image1.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:/Assets/Logo.scale-100.png"));
+            Dimmer.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
     }
 }
